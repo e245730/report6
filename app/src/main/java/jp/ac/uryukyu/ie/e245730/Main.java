5ef9b232c4e648c8cbc515d5bb63c3e4bfbc771d
toguchisouho@toguchikanadefunonotobukkukonpyuta <e245730@ie.u-ryukyu.ac.jp>
2025-01-11 15:05:37 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e245730/Main.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.ie.e245730;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World3!");
+       
+    }
+}
